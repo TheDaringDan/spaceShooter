46fadddbaac7bb9276a9ca8183447e356c5fdc00
Danmasta97 <Danmasta97@10.156.8.102>
2013-11-26 16:39:52 -0500

    Vue class: Le cursor disparait

--- src/ca/qc/bdeb/C37/tp2/Vue.java
+++ src/ca/qc/bdeb/C37/tp2/Vue.java
@@ -3,24 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ca.qc.bdeb.C37.tp2;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
+import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -30,14 +27,15 @@ import javax.swing.JPanel;
  *
  * @author Danmasta97
  */
-public class Vue implements MouseMotionListener, MouseListener{
+public class Vue implements MouseMotionListener, MouseListener {
+
     private int mouseX;
     private int mouseY;
     Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
     JFrame frame;
     JLabel player;
-    
-    public Vue(){
+
+    public Vue() {
         mouseX = 0;
         mouseY = 0;
         JPanel panel = new JPanel();
@@ -46,20 +44,25 @@ public class Vue implements MouseMotionListener, MouseListener{
         panel.addMouseMotionListener(this);
         panel.setBackground(Color.black);
         
+        Cursor invisibleCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+                new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB),
+                new Point(0, 0), "Invisible");
+        frame.setCursor(invisibleCursor);
+
         BufferedImage playerImg;
         try {
             playerImg = ImageIO.read(new File("res/player.png"));
-        } catch(IOException e){
+        } catch (IOException e) {
             playerImg = null;
         }
         BufferedImage playerSprite[] = splitImage(playerImg, 4, 2);
-        
-        player =  new JLabel(new ImageIcon(playerSprite[0]));
+
+        player = new JLabel(new ImageIcon(playerSprite[0]));
         panel.add(player);
-        player.setBounds(225,550,50,50);
+        player.setBounds(225, 550, 50, 50);
         frame.add(panel);
-        frame.setLocation(dim.width/2-250, 10);
-        
+        frame.setLocation(dim.width / 2 - 250, 10);
+
         frame.setSize(500, 700);
         frame.setVisible(true);
         frame.setResizable(false);
@@ -75,17 +78,17 @@ public class Vue implements MouseMotionListener, MouseListener{
     public void mouseMoved(MouseEvent me) {
         mouseX = me.getX();
         mouseY = me.getY();
-        if(mouseX < 45){
+        if (mouseX < 45) {
             mouseX = 45;
         } else if (mouseX > 500 - 45) {
             mouseX = 500 - 45;
         }
-        if(mouseY < 45){
+        if (mouseY < 45) {
             mouseY = 45;
         } else if (mouseY > 700 - 45) {
             mouseY = 700 - 45;
         }
-        player.setBounds(mouseX-25,mouseY-40,50,50);
+        player.setBounds(mouseX - 25, mouseY - 40, 50, 50);
     }
 
     @Override
@@ -105,40 +108,40 @@ public class Vue implements MouseMotionListener, MouseListener{
 
     @Override
     public void mouseEntered(MouseEvent me) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
     public void mouseExited(MouseEvent me) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     /**
-     * Classe qui sépare une 'sprite sheet' en tableau d'images
-     * Trouvé en ligne : http://www.javalobby.org/articles/ultimate-image/
-     * dans la section 'Splitting Images'
-     * 
+     * Classe qui sépare une 'sprite sheet' en tableau d'images Trouvé en ligne
+     * : http://www.javalobby.org/articles/ultimate-image/ dans la section
+     * 'Splitting Images'
+     *
      * @param img La 'sprite sheet' au complet
      * @param cols Le nombre de collones dans la 'sprite sheet'
      * @param rows Le nombre de rangées dans la 'sprite sheet'
      * @return Un tableau d'images (sprites)
      */
-    public static BufferedImage[] splitImage(BufferedImage img, int cols, 
-            int rows) {  
-        int w = img.getWidth()/cols;  
-        int h = img.getHeight()/rows;  
-        int num = 0;  
-        BufferedImage imgs[] = new BufferedImage[w*h];  
-        for(int y = 0; y < rows; y++) {  
-            for(int x = 0; x < cols; x++) {  
-                imgs[num] = new BufferedImage(w, h, img.getType());  
+    public static BufferedImage[] splitImage(BufferedImage img, int cols,
+            int rows) {
+        int w = img.getWidth() / cols;
+        int h = img.getHeight() / rows;
+        int num = 0;
+        BufferedImage imgs[] = new BufferedImage[w * h];
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                imgs[num] = new BufferedImage(w, h, img.getType());
                 // Tell the graphics to draw only one block of the image  
-                Graphics2D g = imgs[num].createGraphics();  
-                g.drawImage(img, 0, 0, w, h, w*x, h*y, w*x+w, h*y+h, null);  
-                g.dispose();  
-                num++;  
-            }  
-        }  
-        return imgs;  
+                Graphics2D g = imgs[num].createGraphics();
+                g.drawImage(img, 0, 0, w, h, w * x, h * y, w * x + w, h * y + h, null);
+                g.dispose();
+                num++;
+            }
+        }
+        return imgs;
     }
 }
